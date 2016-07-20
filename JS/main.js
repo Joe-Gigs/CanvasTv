9bdbbfa66052bb70c6dd6538c827070b32794607
Joe Gigs <jgiglio86@gmail.com>
2016-07-20 16:33:14 -0400

    looks more like a tv

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,21 @@
 window.onload = function() {
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
     var current_channel = Cookies.get('channel_select');
-    // if (current_channel) {
-    //     document.getElementById('greet').innerHTML = current_channel;
-    // }
+
+    canvas.style.backgroundColor = "black";
     console.log(current_channel);
+    document.getElementById('greet').innerHTML = "Off :<";
 };
 
 function draw() {
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
+    canvas.style.backgroundColor = "white";
+    document.getElementById('greet').innerHTML = "Welcome";
     static();
     loop();
-    console.log('on');
-}
+};
 
 function static() {
     var canvas = document.getElementById('myCanvas');
@@ -28,24 +31,21 @@ function static() {
         if (Math.random() < 0.5) buffer32[i] = 0xff000000;
 
     ctx.putImageData(idata, 0, 0);
-}
+};
 
 function clear() {
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
-    canvas.style.backgroundColor = "yellow";
-    console.log('off');
-}
 
-//will play last watched channel when turning the tv on
+    void ctx.clearRect(x, y, width, height);
+    console.log('off');
+};
+
 function rememberChannel() {
     var channel = document.getElementById('select').value;
     Cookies.set('channel_select', channel);
     var current_channel = Cookies.get('channel_select');
-    if (current_channel) {
-        document.getElementById('greet').innerHTML = current_channel;
-    }
-    console.log(current_channel);
+    var num = console.log(current_channel);
     checkChannel();
 };
 
@@ -68,22 +68,32 @@ function checkChannel() {
     var ctx = canvas.getContext('2d');
     var current_channel = Cookies.get('channel_select');
 
-    if (current_channel == 1) {
-        draw();
-        canvas.style.backgroundColor = "yellow";
-    }
-    else if (current_channel == 2) {
-        draw();
-        canvas.style.backgroundColor = "blue";
-    }
-    else if (current_channel == 3) {
-        draw();
-        canvas.style.backgroundColor = "red";
-    } else{
-       
+    switch (current_channel) {
+        case '1':
+            draw();
+            canvas.style.backgroundColor = 'yellow';
+            document.getElementById('greet').innerHTML = "You are viewing channel " + current_channel;
+            break;
+        case '2':
+            draw();
+            canvas.style.backgroundColor = 'blue';
+            document.getElementById('greet').innerHTML = "You are viewing channel " + current_channel;
+            break;
+        case '3':
+            draw();
+            canvas.style.backgroundColor = 'red';
+            document.getElementById('greet').innerHTML = "You are viewing channel " + current_channel;
+            break;
+        case '4':
+            draw();
+            canvas.style.backgroundColor = "white";
+            document.getElementById('greet').innerHTML = "You are viewing channel TEST"; // need to clear canvas
+            break
+        default:
+            document.getElementById('greet').innerHTML = ":D";
     }
 };
 
-function checkIsOn(){
-   
-}
+function turnOff() {
+    location.reload(true)
+};
