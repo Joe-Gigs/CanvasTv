f5ce328816597dbddac828a9ce39bbdd04f0aa0b
Joe Gigs <jgiglio86@gmail.com>
2016-07-21 23:34:51 -0400

    cookies.js gone, working on up/down buttons

--- JS/main.js
+++ JS/main.js
@@ -1,25 +1,25 @@
 var enabled = true;
+var channelCount; 
 
 window.onload = function() {
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
-    var current_channel = Cookies.get('channel_select');
 
     canvas.style.backgroundColor = "black";
-    console.log(current_channel);
     document.getElementById('greet').innerHTML = "Off :(";
 };
 
 function draw() {
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
+    channelCount = 0;
 
     if (enabled == true){
     canvas.style.backgroundColor = "white";
     document.getElementById('greet').innerHTML = "Welcome";
     static();
     loop();
-    // console.log('true');
+    console.log(channelCount);
     } else {
         canvas.style.backgroundColor = "black";
     }
@@ -41,15 +41,6 @@ function static() {
     ctx.putImageData(idata, 0, 0);
 };
 
-function rememberChannel() {
-    var channel = document.getElementById('select').value;
-    var current_channel = Cookies.get('channel_select');
-    var num = console.log(current_channel);
-
-    Cookies.set('channel_select', channel);
-    checkChannel();
-};
-
 var toggle = true;
 
 function loop() {
@@ -67,19 +58,24 @@ function loop() {
 function checkChannel() {
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
-    var current_channel = Cookies.get('channel_select');
+    var current_channel = document.getElementById('select').value;
+
 
     switch (current_channel) {
         case '1':
             canvas.style.backgroundImage = "";
             draw();
             canvas.style.backgroundColor = 'yellow';
+            channelCount = 1;
+            console.log(channelCount);
             document.getElementById('greet').innerHTML = "You are viewing channel " + current_channel;
             break;
         case '2':
             canvas.style.backgroundImage = "";
             draw();
             canvas.style.backgroundColor = 'blue';
+            channelCount = 2;
+            console.log(channelCount);
             document.getElementById('greet').innerHTML = "You are viewing channel " + current_channel;
             break;
         case '3':
@@ -87,17 +83,23 @@ function checkChannel() {
             draw();
             canvas.style.backgroundColor = 'red';
             canvas.style.color = 'black';
+            channelCount = 3;
+            console.log(channelCount);
             document.getElementById('greet').innerHTML = "You are viewing channel " + current_channel;
             break;
         case '4':
             draw();
             canvas.style.backgroundColor = 'black';
             canvas.style.backgroundImage = "url('IMG/01.gif')";
-            document.getElementById('greet').innerHTML = "A E S T H E T I C S "; // need to clear canvas
+            channelCount = 4;
+            console.log(channelCount);
+            document.getElementById('greet').innerHTML = "A E S T H E T I C S "; 
             break
         case '5':
             canvas.style.backgroundColor = null
             canvas.style.backgroundImage = "url('IMG/jtrain.jpg')";
+            channelCount = 5;
+            console.log(channelCount);
             break;
         default:
             document.getElementById('greet').innerHTML = ":D";
@@ -105,7 +107,6 @@ function checkChannel() {
 };
 
 function turnOff() {
-    // location.reload(true)
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
 
@@ -115,24 +116,22 @@ function turnOff() {
 
 };
 
-var count = 0; //possible new channel system. 
-
 function channelUp() {
-    var current_channel = Cookies.get('channel_select');
+    var current_channel = channelCount.value;
 
-    current_channel++;
-    count++;
-    console.log(count);
-    // console.log(current_channel);
+    channelCount++;
+    console.log(channelCount);
+    checkChannel();
+
 };
 
 function channelDown() {
-    var current_channel = Cookies.get('channel_select');
+    var current_channel = channelCount.value;
 
-    current_channel--;
-    count--;
-    console.log(count);
-    // console.log(current_channel);
+    channelCount--;
+    console.log(channelCount);
+    checkChannel();
+
 };
 
 
