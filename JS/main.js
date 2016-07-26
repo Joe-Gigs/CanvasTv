77e1b3f0e8a87d6f329a7d2144ab7e4864345658
Joe Gigs <jgiglio86@gmail.com>
2016-07-26 12:09:39 -0400

    -_-

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,5 @@
 var enabled = false;
-var channelCount; 
+var counter; 
 
 window.onload = function() {
     var canvas = document.getElementById('myCanvas');
@@ -7,16 +7,20 @@ window.onload = function() {
 
     canvas.style.backgroundColor = "black";
     document.getElementById('greet').innerHTML = "Off :(";
+    channelUpDown(); 
 };
 
 function draw() {
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
-    var current_channel = document.getElementById('select').value
-    current_channel = 0;
+    // var current_channel = document.getElementById('select').value
+    counter = 0;
+    console.log(counter);
     enabled = true;
     var x = document.getElementById('up');
     var y = document.getElementById('down');
+    var z = document.getElementById('remember');
+    z.disabled = false;
     y.disabled = false;
     x.disabled = false;
     if (enabled == true){
@@ -24,10 +28,14 @@ function draw() {
     document.getElementById('greet').innerHTML = "Welcome";
     static();
     loop();
-    console.log(current_channel);
+    // console.log(current_channel);
     } else {
         canvas.style.backgroundColor = "black";
     }
+    var check = document.getElementById('remember');
+    check.addEventListener('click', function(){
+        checkChannel();
+    }) 
 };
 
 function static() {
@@ -63,47 +71,43 @@ function loop() {
 function checkChannel() {
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
-    var current_channel = document.getElementById('select').value
+    var current_channel = document.getElementById('select').value; //takes in channel input//
 
-    switch (current_channel) {
+       switch(current_channel) {
         case '1':
             canvas.style.backgroundImage = "";
-            draw();
             canvas.style.backgroundColor = 'yellow';
-            channelCount = 1;
-            console.log(channelCount);
+            counter = '1';
+            console.log(counter);
             document.getElementById('greet').innerHTML = "You are viewing channel " + current_channel;
             break;
         case '2':
             canvas.style.backgroundImage = "";
-            draw();
             canvas.style.backgroundColor = 'blue';
-            channelCount = 2;
-            console.log(channelCount);
+            counter = '2';
+            console.log(counter);
             document.getElementById('greet').innerHTML = "You are viewing channel " + current_channel;
             break;
         case '3':
             canvas.style.backgroundImage = "";
-            draw();
             canvas.style.backgroundColor = 'red';
             canvas.style.color = 'black';
-            channelCount = 3;
-            console.log(channelCount);
+            counter = 3;
+            console.log(counter);
             document.getElementById('greet').innerHTML = "You are viewing channel " + current_channel;
             break;
         case '4':
-            draw();
             canvas.style.backgroundColor = 'black';
             canvas.style.backgroundImage = "url('IMG/01.gif')";
-            channelCount = 4;
-            console.log(channelCount);
+            counter = 4;
+            console.log(counter);
             document.getElementById('greet').innerHTML = "A E S T H E T I C S "; 
             break
         case '5':
             canvas.style.backgroundColor = null
             canvas.style.backgroundImage = "url('IMG/jtrain.jpg')";
-            channelCount = 5;
-            console.log(channelCount);
+            counter = 5;
+            console.log(counter);
             break;
         default:
             document.getElementById('greet').innerHTML = ":D";
@@ -115,8 +119,11 @@ function turnOff() {
     var ctx = canvas.getContext('2d');
     var x = document.getElementById('up');
     var y = document.getElementById('down')
+    var z = document.getElementById('remember');
+
     x.disabled = true;
     y.disabled = true;
+    z.disabled = true;
 
     canvas.style.backgroundColor = 'black';
     canvas.style.backgroundImage = "";
@@ -124,22 +131,19 @@ function turnOff() {
 
 };
 
-function channelUp() {
-    var current_channel = document.getElementById('select').value
+function channelUpDown() {
+    var current_channel = document.getElementById('select').value;
+    var channel_up = document.getElementById('up');
+     channel_up.addEventListener('click', function(){
+        current_channel++;
+        console.log(current_channel);
+    }, false);
 
-    current_channel++;
-    console.log(current_channel);
-    // checkChannel();
-
-};
-
-function channelDown() {
-    var current_channel = document.getElementById('select').value
-
-    current_channel--;
-    console.log(current_channel);
-    // checkChannel();
-
+    var channel_down = document.getElementById('down');
+    channel_down.addEventListener('click', function(){
+        current_channel--;
+        console.log(current_channel);
+    }, false);
 };  
 
 
